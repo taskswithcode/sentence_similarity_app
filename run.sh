7ba09c4c3489966f195f86362da11636d0befefc
taskswithcode <farminaround@gmail.com>
2022-09-14 21:44:55 +0200

    autorestart

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 ##streamlit run app.py --server.port 80
-streamlit run app.py --server.port 8501  --server.enableCORS false --server.enableXsrfProtection false
+
+count=0
+while :
+do
+    streamlit run app.py --server.port 8501  --server.enableCORS false --server.enableXsrfProtection false
+    ((count=count+1))
+    echo "Restarted $count times"
+done
